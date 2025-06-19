21502436d21fc63126f1d8bed818d40e753469ad
jabeena <jabina123@gmail.com>
2025-06-19 18:07:41 +0300

    Fix wildcard route for production

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get("/api/config/paypal", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
+  app.get("/*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
